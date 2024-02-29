5eb020c9c0728abb936521f81642917c35621565
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-29 17:40:13 +0200

    Add files via upload

---
+++ Problem1.js
@@ -0,0 +1,28 @@
+function personalData (firstName, lastName, age){
+let firstResult= {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+}
+
+// let secondResult = {};
+//     secondResult.firstName=firstName;
+//     secondResult.lastName=lastName;
+//     secondResult.age=age;
+
+// let thirdResult = {
+//     firstName, 
+//     lastName,
+//     age,
+// }
+
+return firstResult
+}
+
+    
+
+personalData("Peter", 
+"Pan",
+"20"
+);
+// personalData();
